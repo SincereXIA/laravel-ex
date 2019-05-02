e26fff31a792acaafbcd03a3dd206a2b528aa15f
SincereXIA <SincereXIA@users.noreply.github.com>
2019-05-02 11:47:18 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
                 <div class="title">Pass 服务实例</div>
                 <p>西安电子科技大学 云计算课程</p>
                 <p>张俊华 16030199025</p>
+                <p><a href="http://tz-cloudtest.4b63.pro-ap-southeast-2.openshiftapps.com/tz.php">点击进入探针</a></p>
             </div>
         </div>
     </body>
